b6091ccd110af8e3d9828cb81a7a6f4fed78af99
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-08-25 17:51:01 +0100

    Added and got passing tests for zone fare deductions

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -13,6 +13,24 @@ describe Journey do
 
   describe '#fare' do
 
+    it 'gives us the correct fare when travelling between zone 2 and zone 6' do
+      journey1 = Journey.new(station, 2)
+      journey1.set_end_station(station, 6)
+      expect(journey1.fare).to eq 5
+    end
+
+    it 'gives us the correct fare when travelling between zone 6 and zone 2' do
+      journey1 = Journey.new(station, 6)
+      journey1.set_end_station(station, 2)
+      expect(journey1.fare).to eq 5
+    end
+
+    it 'gives us the correct fare when travelling within the same zone' do
+      journey1 = Journey.new(station, 1)
+      journey1.set_end_station(station, 1)
+      expect(journey1.fare).to eq 1
+    end
+
     it 'calculates the fare' do
       subject.set_end_station(station, zone)
       expect(subject.fare).to eq Journey::MIN_FARE
