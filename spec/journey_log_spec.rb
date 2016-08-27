6a2559b4f5a275558eefee8cd55f7917deb5d1e1
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-08-27 21:23:47 +0100

    some changes

--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -2,12 +2,15 @@ require 'journey_log'
 
 describe JourneyLog do
 
-  subject(:journey_log) { described_class.new(journey_class: journey_class) }
-  let(:journey_class) { double :journey_class, new: journey }
-  let(:journey) { double :journey, entry_station: entry_station, exit_station: exit_station }
-  # let(:journey) { double :journey }
   let(:entry_station) { double :station }
   let(:exit_station) { double :station }
+
+  subject(:journey_log) { described_class.new(journey_class: journey_class) } #This creates a new instance of the journey_log class.
+
+  let(:journey) { double :journey, entry_station: entry_station, exit_station: exit_station } #This creates a double of the journey IN PROGRESS.
+
+  let(:journey_class) { double :journey_class, new: journey } #This tells the journey_log @journey_class intance variable to respond to the ".new" call with the journey (IN PROGRESS) double that was made on line 10.
+
 
   describe '#start' do
     it 'should return a journey with an entry station' do
